3694d16ebe264f2cf8378c0894c1a6122d612ccf
nsk2k04 <naveenkumarsampath2004@gmail.com>
2026-06-21 11:18:19 +0530

    Fix Vercel CORS

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
         "https://uni-sphere-rag-pdf-chatbot.vercel.app"
     ],
     allow_credentials=True,
